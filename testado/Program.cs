d8da9bd3ff887c1d23aff79d72926157bb8799fa
Sasha <aleksandr.marfut@gmail.com>
2020-10-05 10:22:04 +0200

    Update Program.cs

--- testado/Program.cs
+++ testado/Program.cs
@@ -5,8 +5,12 @@ namespace testado
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {          
+            Console.WriteLine("Step 1");
+            
+            throw new Exception("Exception from utility")
+            
+            Console.WriteLine("Step 2");
         }
     }
 }
